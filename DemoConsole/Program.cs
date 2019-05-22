b518574786b1e7f871b7d78a72cf0c8153f0d499
Артём Григорьян <artem.grigorian2002@gmail.com>
2019-05-22 19:20:54 +0300

    change execution parametrs

--- DemoConsole/Program.cs
+++ DemoConsole/Program.cs
@@ -17,14 +17,14 @@ namespace DemoConsole
             var startTime = new DateTime(2019,3,27);
 
             var orders = new Queue<Order>();
-            for (var i = 0; i < 300000; i++)
+            for (var i = 0; i < 10000; i++)
             {
                 orders.Enqueue(
                     new Order(TimeSpan.FromMinutes(30), TimeSpan.FromSeconds(300),
                         startTime.AddMinutes(3 * i), i));
             }
 
-            var groupDeliverers = Enumerable.Range(1, 10 * 2).Select(i => new Deliverer(i)).ToList();
+            var groupDeliverers = Enumerable.Range(1, 20).Select(i => new Deliverer(i)).ToList();
 
             var currentTime = startTime;
 
