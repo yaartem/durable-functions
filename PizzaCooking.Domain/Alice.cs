52e3230a832e61588f7bb96b78a06d2e4725dfbf
Artem <artem.grigorian2002@gmail.com>
2019-06-14 20:50:23 +0300

    refactor Alice out

--- PizzaCooking.Domain/Alice.cs
+++ PizzaCooking.Domain/Alice.cs
@@ -12,88 +12,70 @@ namespace PizzaCooking.Domain
         private static List<string> HalfWords { get; }
         private static List<string> FinishWords { get; }
         private List<string> ManyOrdersWords { get; }
-        private static  List<string> NearlyEndWords { get; }
+        private static List<string> NearlyEndWords { get; }
 
         public DateTime CurrentTime { get; set; }
         public DateTime FraseSaid { get; set; }
-        
+
         public IEnumerable<int> GetSequence()
         {
             while (CurrentTime.TimeOfDay < TimeSpan.FromHours(9))
             {
                 yield return 0;
             }
-                Say($"{CurrentTime} {HelloWords[_rnd.Next(0, 4)]}");
-                yield return 0;
-            while (CurrentTime.TimeOfDay < TimeSpan.FromHours(10))
-            {
-                PhrasesAll(CurrentlyInProcessing,GroupPizzamakers);
-                yield return 0;
-            }
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                BlameLaties(GroupPizzamakers);
-                Console.ForegroundColor = ConsoleColor.White;
-                yield return 0;
+
+            Say($"{CurrentTime} {HelloWords[_rnd.Next(0, 4)]}");
+            yield return 0;
             while (CurrentTime.TimeOfDay < TimeSpan.FromHours(13))
             {
                 PhrasesAll(CurrentlyInProcessing, GroupPizzamakers);
                 yield return 0;
             }
-                
-                Say(string.Format("{0} {1}", CurrentTime, HalfWords[_rnd.Next(0, 2)]));
-                
-                CurrentTime = CurrentTime.AddMinutes(30);
-                FraseSaid = CurrentTime;
-                yield return 0;
+            Say(string.Format("{0} {1}", CurrentTime, HalfWords[_rnd.Next(0, 2)]));
+            CurrentTime = CurrentTime.AddMinutes(30);
+            FraseSaid = CurrentTime;
+            yield return 0;
             while (CurrentTime.TimeOfDay < TimeSpan.FromHours(22))
             {
                 PhrasesAll(CurrentlyInProcessing, GroupPizzamakers);
                 yield return 0;
             }
-                
-                Say(string.Format("{0} {1}", CurrentTime, NearlyEndWords[_rnd.Next(0, 2)]));
-                FraseSaid = CurrentTime;
-                yield return 0;
+            Say(string.Format("{0} {1}", CurrentTime, NearlyEndWords[_rnd.Next(0, 2)]));
+            FraseSaid = CurrentTime;
+            yield return 0;
             while (CurrentTime.TimeOfDay < TimeSpan.FromHours(23))
             {
                 PhrasesAll(CurrentlyInProcessing, GroupPizzamakers);
                 yield return 0;
             }
-                
-                Say(FinishWords[_rnd.Next(0, 2)]);
-                Say(string.Format("Есть люди как люди, а есть {0}! {1}!Слишком много заказов!",
-                    GroupPizzamakers[BestPizzamaker(GroupPizzamakers)].Name, GroupPizzamakers[BestPizzamaker(GroupPizzamakers)].OrdersDone));
-                
-                foreach (var man in GroupPizzamakers)
+            Say(FinishWords[_rnd.Next(0, 2)]);
+            Say(string.Format("Есть люди как люди, а есть {0}! {1}!Слишком много заказов!",
+                GroupPizzamakers[BestPizzamaker(GroupPizzamakers)].Name, GroupPizzamakers[BestPizzamaker(GroupPizzamakers)].OrdersDone));
+            foreach (var man in GroupPizzamakers)
+            {
+                if (man.OrdersDone > 20)
                 {
-                    if (man.OrdersDone > 20)
+                    Say($"Большой молодец-{man.Name}, {man.OrdersDone} выполнено заказов");
+                }
+                else
+                {
+                    if (man.OrdersDone > 10)
                     {
-                        Say($"Большой молодец-{man.Name}, {man.OrdersDone} выполнено заказов");
+                        Say($"Молодец-{man.Name}, {man.OrdersDone} выполнено заказов");
                     }
                     else
                     {
-                        if (man.OrdersDone > 10)
-                        {
-                            Console.WriteLine("Молодец-{0}, {1} выполнено заказов", man.Name, man.OrdersDone);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Ничего так-{0}, {1} выполнено заказов", man.Name, man.OrdersDone);
-                            Console.WriteLine("Шучу, умница ты, {0}", man.Name);
-                        }
+                        Say($"Ничего так-{man.Name}, {man.OrdersDone} выполнено заказов");
                     }
                 }
-                Console.WriteLine();
-                Console.ForegroundColor = ConsoleColor.White;
+            }
+            yield return 0;
+            while (CurrentlyInProcessing.Count > 0)
+            {
                 yield return 0;
-                while (CurrentlyInProcessing.Count>0)
-                {
-                    yield return 0;
-                }
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine("Все доставлено и сделано! Ждем следующего дня");
-                Console.ForegroundColor = ConsoleColor.White;
-                yield return 0;
+            }
+            Say("Все доставлено и сделано! Ждем следующего дня");
+            yield return 0;
         }
 
         private void Say(string what)
@@ -149,29 +131,6 @@ namespace PizzaCooking.Domain
             };
         }
 
-        public void BlameLaties(List<Pizzamaker> guys)
-        {
-            string laties = " ";
-            foreach (var i in guys)
-            {
-                if (i.IsLate())
-                {
-                    if (laties == " ")
-                    {
-                        laties += i.Name;
-                    }
-                    else
-                    {
-                        laties = laties + ", " + i.Name;
-                    }
-                }
-            }
-            if (laties != " ")
-            {
-                Console.WriteLine("Друзья мои,{0}, опаздываем! В следующий раз так не обижайте меня", laties);
-            }
-
-        }
 
         public int BestPizzamaker(List<Pizzamaker> guys)
         {
@@ -188,36 +147,29 @@ namespace PizzaCooking.Domain
         {
             if (CurrentTime >= FraseSaid.AddMinutes(60))
             {
-                Console.ForegroundColor = ConsoleColor.DarkRed;
                 switch (_rnd.Next(1, 6))
                 {
                     case 1:
-                        Console.WriteLine("{0} выполнил больше всех заказов! Можно и перерыв на кофе",
-                            groupPizzamakers[BestPizzamaker(groupPizzamakers)].Name);
+                        Say($"{groupPizzamakers[BestPizzamaker(groupPizzamakers)].Name} выполнил больше всех заказов! Можно и перерыв на кофе");
                         FraseSaid = CurrentTime;
                         break;
                     case 2:
-                        Console.WriteLine("Новый король пиццы-{0}, больше всего заказов за сегодня!",
-                            groupPizzamakers[BestPizzamaker(groupPizzamakers)].Name);
+                        Say($"Новый король пиццы-{groupPizzamakers[BestPizzamaker(groupPizzamakers)].Name}, больше всего заказов за сегодня!");
                         FraseSaid = CurrentTime;
                         break;
                     default:
                         break;
                 }
-                Console.ForegroundColor = ConsoleColor.White;
             } //Хвальба лучших
 
             if (currentlyInProcess.Count > 10 && CurrentTime >= FraseSaid.AddMinutes(15))
             {
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine("{0} {1}", CurrentTime, ManyOrdersWords[_rnd.Next(0, 3)]);
-                Console.ForegroundColor = ConsoleColor.White;
+                Say($"{CurrentTime} {ManyOrdersWords[_rnd.Next(0, 3)]}");
                 FraseSaid = CurrentTime;
             } //Много заказов
 
             foreach (var item in currentlyInProcess)
             {
-                Console.ForegroundColor = ConsoleColor.DarkRed;
                 if (item.order.State == "Is Waiting To Be Cooked" && !item.order.TakenToCook)
                 {
                     if (item.order.Pizzas >= 3 || item.order.AnyDrinks)
@@ -227,17 +179,13 @@ namespace PizzaCooking.Domain
                             switch (_rnd.Next(1, 4))
                             {
                                 case 1:
-                                    Console.WriteLine("Кто так много ест? Заказ№{0}! Там больше трех пицц",
-                                        item.order.OrderNumber);
+                                    Say($"Кто так много ест? Заказ№{item.order.OrderNumber}! Там больше трех пицц");
                                     break;
                                 case 2:
-                                    Console.WriteLine(
-                                        "Какой-то праздник намечается? Тогда откуда больше 3 пицц в заказе№{0}?",
-                                        item.order.OrderNumber);
+                                    Say($"Какой-то праздник намечается? Тогда откуда больше 3 пицц в заказе№{item.order.OrderNumber}?");
                                     break;
                                 default:
-                                    Console.WriteLine("Заказ№{1} <<{0}>>-многовато пицц для одного заказа",
-                                        item.order.Content, item.order.OrderNumber);
+                                    Say($"Заказ№{item.order.OrderNumber} <<{item.order.Content}>>-многовато пицц для одного заказа");
                                     break;
                             }
                         }
@@ -246,14 +194,13 @@ namespace PizzaCooking.Domain
                             switch (_rnd.Next(1, 4))
                             {
                                 case 1:
-                                    Console.WriteLine("Напитки в заказе! Наверное, в пустыню едет...");
+                                    Say("Напитки в заказе! Наверное, в пустыню едет...");
                                     break;
                                 case 2:
-                                    Console.WriteLine("Заказ№{0} с напитками, не забудьте", item.order.OrderNumber);
+                                   Say($"Заказ№{item.order.OrderNumber} с напитками, не забудьте");
                                     break;
                                 default:
-                                    Console.WriteLine("Стоит отметить, что заказ№{1} <<{0}>> имеет напитки",
-                                        item.order.Content, item.order.OrderNumber);
+                                    Say($"Стоит отметить, что заказ№{item.order.Content} <<{item.order.OrderNumber}>> имеет напитки");
                                     break;
                             }
                         }
@@ -263,16 +210,13 @@ namespace PizzaCooking.Domain
                         switch (_rnd.Next(1, 4))
                         {
                             case 1:
-                                Console.WriteLine("Господа, заказ <<{0}>> №{1} не готов! Пора бы им заняться",
-                                    item.order.Content, item.order.OrderNumber);
+                                Say($"Господа, заказ <<{item.order.Content}>> №{item.order.OrderNumber} не готов! Пора бы им заняться");
                                 break;
                             case 2:
-                                Console.WriteLine("<<{0}>>, заказ №{1} и пиццамейкер! Надо бы сделать",
-                                    item.order.Content, item.order.OrderNumber);
+                                Say($"<<{item.order.Content}>>, заказ №{item.order.OrderNumber} и пиццамейкер! Надо бы сделать");
                                 break;
                             case 3:
-                                Console.WriteLine("<<{0}>>, заказ №{1}. За работу, дорогие!", item.order.Content,
-                                    item.order.OrderNumber);
+                                Say($"<<{item.order.Content}>>, заказ №{item.order.OrderNumber}. За работу, дорогие!");
                                 break;
                             default:
                                 break;
@@ -285,21 +229,16 @@ namespace PizzaCooking.Domain
                     switch (_rnd.Next(1, 3))
                     {
                         case 1:
-                            Console.WriteLine("Травка зеленеет, солнышко блестит, заказ№{0} к курьерам летит!",
-                                item.order.OrderNumber);
+                            Say($"Травка зеленеет, солнышко блестит, заказ№{item.order.OrderNumber} к курьерам летит!");
                             break;
                         case 2:
-                            Console.WriteLine("<<{0}>> выполнен! Доставщики, ваш выход", item.order.Content);
+                            Say($"<<{item.order.Content}>> выполнен! Доставщики, ваш выход");
                             break;
                         default:
                             break;
                     }
-                }
-                Console.ForegroundColor = ConsoleColor.White;
+                }Console.ForegroundColor = ConsoleColor.White;
             }
         }
-
-        
-
     }
 }
